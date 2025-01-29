4f2bb587caf00fde8263c028282cbef3066271c8
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-29 15:55:15 +0100

    j'ai terminé la partie burrata et la partie linguines à la carbonara

--- menu.php
+++ menu.php
@@ -16,7 +16,7 @@
     <header class="tete">
         <ul>
             <li><a href="menu.php"><img src="images/menu.png" alt="logo" height="50" width="50"></a></li>
-            <li><a href="index.php"><div class="titre"><h1 class="titre">MAMMA MIA</h1></div></a></li>
+            <li><a href="index.php"><h1 class="titre">MAMMA MIA</h1></a></li>
             <li><a href="deconnexion.php"><img src="images/connexion.png" alt="logo" height="50" width="50"></a></li>
         </ul>
     </header>
@@ -24,17 +24,32 @@
 <!--MENU-->
     <section class="menu">
         <div class="entrée1">
-
+            <div class="entrée2">
+                <img class="burrata" src="images/burrata.png" alt="antipasti">
+                <h3 class="burrata2">Burrata à la provençale</h3>
+            </div>
         </div>
 
         <div class="plat1">
-
+            <div class="plat2">
+                <img class="linguine" src="images/linguines à la carbonara.png" alt="linguines à la carbonara">
+                <h3 class="linguine2">Linguines à la carbonara</h3>
         </div>
 
         <div class="dessert1">
 
         </div>
     </section>
+
+<!--FOOTER-->
+    <footer>
+        <div class="bas">
+            <a href="index.php">Accueil</a>
+            <a href="menu.php">Menu</a>
+            <a href="connexion.php">Connexion</a>
+        </div>
+    </footer>
+
 
 </body>
 </html>
