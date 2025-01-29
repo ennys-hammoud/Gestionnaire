84e89d35635abc0e838b4fee1a16d242cbe51b84
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-29 14:13:19 +0100

    j'ai ajouté le header sur la page menu

---
+++ menu.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!--HEADER-->
+    <header class="tete">
+        <ul>
+            <li><a href="menu.php"><img src="images/menu.png" alt="logo" height="50" width="50"></a></li>
+            <li><a href="index.php"><div class="titre"><h1 class="titre">MAMMA MIA</h1></div></a></li>
+            <li><a href="deconnexion.php"><img src="images/connexion.png" alt="logo" height="50" width="50"></a></li>
+        </ul>
+    </header>
+
+<!--MENU-->
+    <section class="menu">
+        <div class="entrée1">
+
+        </div>
+
+        <div class="plat1">
+
+        </div>
+
+        <div class="dessert1">
+
+        </div>
+    </section>
+
+</body>
+</html>
